6253300b0dc9de78f2b0938e34b744b916e8c990
Faiza Sharif <step.faizasharif@gmail.com>
2020-10-20 22:38:41 +0500

    7th commited

---
+++ WhileLoop.java
@@ -0,0 +1,12 @@
+public class WhileLoop
+{
+    public static void main(String[] args)
+    {
+        int count=1;
+        while (count<=10) 
+        {
+            System.out.println("Count is: " + count);
+            count++;
+        }
+    }
+}
