561976cfc438276d4147bd9e890e248d6108aa97
Faiza Sharif <step.faizasharif@gmail.com>
2020-10-20 22:36:21 +0500

    3rd commited

---
+++ Expression.java
@@ -0,0 +1,15 @@
+public class Expression
+{
+    public static void main(String[] args)
+    {
+        int a=50;
+          if (a%2==0)
+          {
+              System.out.println("Number is even.");
+          }
+          else
+          {
+              System.out.println("Number is odd.");
+          }
+    }
+}
