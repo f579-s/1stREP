c675452d61c46d3316e5cfb77589e807db5965ae
Faiza Sharif <step.faizasharif@gmail.com>
2020-10-20 22:35:33 +0500

    commited

---
+++ Euclid.java
@@ -0,0 +1,33 @@
+public class Euclid{
+
+    // recursive implementation
+    public Static int gcd(int p, int q)
+    {
+        if (q == 0) return p;
+        else return gcd(q, p % q);
+    }
+
+    // non-recursive implementation
+    public Static int gcd2(int p, int q)
+       {
+         while (q != 0) {
+            int temp = q;
+            q = p % q;
+            p = temp;
+        }
+        return p;
+    }
+    
+    // main method
+    public static void main(String[] args)
+    {
+        int p = Integer.parseInt(args[0]);
+        int q = Integer.parseInt(args[1]);
+        int d  = gcd(p, q); //resursion
+        int d2 = gcd2(p, q); //while loop
+        system.out.println("gcd(" + p + ", " + q + ") = " + d);
+        system.out.println("gcd(" + p + ", " + q + ") = " + d2);
+    }
+}
+
+
